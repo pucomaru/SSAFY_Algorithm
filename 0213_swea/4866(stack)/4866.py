1f0c3313510b24873664293aad8df590691622aa
daye <myannnng@gmail.com>
2025-02-14 02:10:53 +0900

    0213

--- 0213_swea/4866(stack)/4866.py
+++ 0213_swea/4866(stack)/4866.py
@@ -1,14 +1,32 @@
-
-#import sys
-#sys.stdin = open("input.txt", "r")
+import sys
+sys.stdin = open("sample_input.txt", "r")
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
-    '''
 
-        이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+    sentence = input()
 
-    '''
+    stack = []
+
+    result = 1  # 결과값
+
+    for char in sentence:
+        if char in "({":
+            stack.append(char)
+        if char in ")}":
+            if not stack:
+                result = 0
+            if stack:
+                top = stack.pop()
+                if (char == ")" and top != "(") or (char == "}" and top != "{"):
+                    result = 0
+
+    if stack:
+        result = 0
+
+
+
+    print(f"#{test_case} {result} ")
     # ///////////////////////////////////////////////////////////////////////////////////
