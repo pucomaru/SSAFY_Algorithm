6dcc5fa07d25149deb547ecc27c1d2adfc877160
daye <myannnng@gmail.com>
2025-02-14 20:41:50 +0900

    0214

--- 0214_swea/2805/2805.py
+++ 0214_swea/2805/2805.py
@@ -1,16 +1,13 @@
-
 import sys
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, 2):
+for test_case in range(1, T+1):
     # ///////////////////////////////////////////////////////////////////////////////////
     N = int(input())            # 농장의 크기
 
     crop = [input() for _ in range(N)]     # 농작물 가치 입력
-
-    print(crop)
 
     crop_sum = 0                # 수익 변수
 
@@ -18,17 +15,26 @@ for test_case in range(1, 2):
 
     # 가운데 행을 기준으로 위 아래 나눠서 따로 수익을 더 해준다.
     # 가운데 줄 기준 윗줄 쫙 더하기
+    i = 1
     for width in range(middle_idx-1, -1, -1):         # 7 * 7 이면 width = 2, 1, 0 더하기
-        for i in range(N // 2):                       # 몫 만큼 줄 계산 해야함
-            for height in range(1 + i, (N-1-i)):      # 중간 인덱스 인접 줄은 idx = 1 부터 시작 고정 뒤 idx도 고정
-                crop_sum += int(crop[width][height])
+        for height in range(i, (N-i)):      # 중간 인덱스 인접 줄은 idx = 1 부터 시작 고정 뒤 idx도 고정
+            crop_sum += int(crop[width][height])
+        i += 1
 
-    # 가운데 줄 기준 가운데 줄 포함 아랫줄 쫙 더하기
+    # for width in range(middle_idx-1, -1, -1):         # 7 * 7 이면 width = 2, 1, 0 더하기
+    #     i = 1
+    #     for height in range(i, (N-i)):      # 중간 인덱스 인접 줄은 idx = 1 부터 시작 고정 뒤 idx도 고정
+    #         crop_sum += int(crop[width][height])
+    #         i += 1
+    # for height 문 내에다가 i를 넣으면 범위에 영향이 가서 원하는 결과가 안나옴
+
+   # 가운데 줄 기준 가운데 줄 포함 아랫줄 쫙 더하기
+    j = 0
     for width in range(N//2, N):                      # 7 * 7 이면 width = 3, 4, 5, 6 더하기
-        for i in range(N // 2 + 1):                   # 중간 줄 포함해서 계산하므로 몫 + 1 계산함
-            for height in range(1 + i, (N - 1 - i)):  # 중간 인덱스 인접 줄은 idx = 1 부터 시작 고정 뒤 idx도 고정
-                crop_sum += int(crop[width][height])
+        for height in range(j, (N - j)):  # 중간 인덱스 인접 줄은 idx = 1 부터 시작 고정 뒤 idx도 고정
+            crop_sum += int(crop[width][height])
+        j += 1
 
-    print(f"{test_case} {crop_sum}")
+    print(f"#{test_case} {crop_sum}")
 
     # ///////////////////////////////////////////////////////////////////////////////////
