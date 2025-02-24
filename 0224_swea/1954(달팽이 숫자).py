c64e8634cf4303fc3d6542d2d1e058e19c1597b2
daye <myannnng@gmail.com>
2025-02-24 23:08:04 +0900

    0224

--- 0224_swea/1954(달팽이 숫자).py
+++ 0224_swea/1954(달팽이 숫자).py
@@ -1,47 +1,40 @@
-
-#import sys
-#sys.stdin = open("input.txt", "r")
-
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    
-    N = int(input())                    # 달팽이 크기
-
-    dx = [1, 0  -1, 0]                  # x 증가량
-    dy = [0, 1, 0, -1]                  # y 증가량
-    delta = list(zip(dx,dy))            # 델타
-
-    space = [[0] * N for _ in range(N)]         # 공간
-
-
+    N = int(input())
+    space = [[0] * N for _ in range(N)]
     num = 1
-    while 1:
-        if num > N*N:
-            break
+    top, bottom = 0, N - 1
+    left, right = 0, N - 1
 
-        i = 0
-        j = 0
-
-        for right in range(N):
-            space[i][j] = num
-            i += 1
+    while num <= N * N:
+        # 왼쪽 → 오른쪽
+        for j in range(left, right + 1):
+            space[top][j] = num
             num += 1
-        for down in range(N-1):
-            j += 1
-            space[i][j] = num
-            num += 1
-        for left in range(N-1):
-            i -= 1
-            space[i][j] = num
-            num += 1
-        for up in range(N-2):
-            j -= 1
-            space[i][j] = num
-            num +=1
+        top += 1
 
-        N /= 2
+        # 위쪽 → 아래쪽
+        for i in range(top, bottom + 1):
+            space[i][right] = num
+            num += 1
+        right -= 1
 
-    print(f"{test_case} {}")
-    # ///////////////////////////////////////////////////////////////////////////////////
+        # 오른쪽 → 왼쪽
+        if top <= bottom:
+            for j in range(right, left - 1, -1):
+                space[bottom][j] = num
+                num += 1
+            bottom -= 1
+
+        # 아래쪽 → 위쪽
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                space[i][left] = num
+                num += 1
+            left += 1
+
+    print(f"#{test_case}")
+    for row in space:
+        print(" ".join(map(str, row)))
+
+# 나중에 다시 풀어보기 
