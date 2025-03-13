85a80684f243be1a4435e6541468489ca8a79fba
daye <myannnng@gmail.com>
2025-03-13 23:42:53 +0900

    0313

--- 0313/swea/1247(최적 경로)/1247.py
+++ 0313/swea/1247(최적 경로)/1247.py
@@ -1,23 +1,27 @@
 #import sys
 #sys.stdin = open("input.txt", "r")
 
-# 고객좌표를 싹 돌아봄. visited 이용
-
 from itertools import permutations
 
 def distance(arr):
     global short
     d = 0
 
+    d += abs(firm[0] - arr[0][0]) + abs(firm[1] - arr[0][1])
+
     for i in range(len(customer)-1):
-        d += abs(customer[i][0] - customer[i+1][0]) + abs(customer[i][1] - customer[i+1][1])
+        if d > short :
+            return
+        d += abs(arr[i][0] - arr[i+1][0]) + abs(arr[i][1] - arr[i+1][1])
+
+    d += abs(arr[-1][0] - home[0]) + abs(arr[-1][1] - home[1])
 
     if d < short:
         short = d
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, 2):
+for test_case in range(1, T+1):
     # ///////////////////////////////////////////////////////////////////////////////////
 
     N = int(input())                        # N 고객의 수
@@ -35,19 +39,13 @@ for test_case in range(1, 2):
 
     # 고객좌표 리스트 추가
     for idx in range(4, len(xy), 2):
-        customer.append([idx, idx+1])
-    print(customer)
+        customer.append([xy[idx], xy[idx+1]])
 
-
-    orders = permutations(customer,len(customer))
-
-    for order in orders:
+    # 순열로 모든 경로 경우의 수 구함
+    for order in permutations(customer):
         distance(order)
 
 
     print(f"#{test_case} {short}")
-
-
-
 
     # ///////////////////////////////////////////////////////////////////////////////////
