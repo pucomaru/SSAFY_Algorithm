b65a0a898233b6760564e8c16a2ca957a89be49a
dayea <myannnng@gmail.com>
2025-03-14 17:42:16 +0900

    0314

--- 0314/swea/당근 포장하기( 16811)/re-16811.py
+++ 0314/swea/당근 포장하기( 16811)/re-16811.py
@@ -7,24 +7,54 @@
 # 한 상자에 N//2 를 초과하면 안됨
 # 앞 조건들을 만족하고 각 상자에 든 당근의 개수 차이가 최소가 되도록 포장
 
-def dfs(s, m, l, v):
-
-    
-
-
-
-
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, 2):
-    # ///////////////////////////////////////////////////////////////////////////////////
-    N = int(input())                                                # 당근의 개수
-    size = list(map(int, input().split()))                          # 당근 크기
-    result = 0
+def dfs(s, e):
+    global small
+    global middle
+    global large
+    global result
 
     small = []
     middle = []
     large = []
+
+    if s == e:
+       return
+
+    for i in range(s):
+        small.append(size_arr[i])
+    for j in range(s, e):
+        middle.append(size_arr[j])
+    for k in range(e, len(size_arr)):
+        large.append(size_arr[k])
+
+    s_hap = 0
+    m_hap = 0
+    l_hap = 0
+
+    for idx in small:
+        s_hap += size_arr[idx]
+    for idx in middle:
+        m_hap += size_arr[idx]
+    for idx in large:
+        l_hap += size_arr[idx]
+
+    max_hap = max(s_hap, m_hap, l_hap)
+    min_hap = min(s_hap, m_hap, l_hap)
+
+    difference = max_hap - min_hap
+
+    if difference < result:
+        result = difference
+
+    dfs(s+1, e-1)
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T+1):
+    # ///////////////////////////////////////////////////////////////////////////////////
+    N = int(input())                                                # 당근의 개수
+    size = list(map(int, input().split()))                          # 당근 크기
+    result = 1e9
 
     # 당근 크기는 1 ~ 30
     how_many = [0] * 31
@@ -33,9 +63,13 @@ for test_case in range(1, 2):
     for i in size:
         how_many[i] += 1
 
-    print(how_many)
+    size_arr = []
 
-    dfs(small, middle, large, how_many)
+    for i in range(len(how_many)):
+        if how_many[i] > 0:
+            size_arr.append(i)
+
+    dfs(1, len(size_arr)-1)
 
     print(f"#{test_case} {result}")
     # ///////////////////////////////////////////////////////////////////////////////////
