d351fcb557986d777b29dc2e633f9a11b6a7443b
pucomaru <myannnng@gmail.com>
2025-03-19 17:54:21 +0900

    0319

--- 0319/swea/5248(그룹 나누기)/5248.py
+++ 0319/swea/5248(그룹 나누기)/5248.py
@@ -2,11 +2,30 @@
 import sys
 sys.stdin = open("sample_input.txt", "r")
 
+
+
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
 
-    N, M = mapint,input().split())
+    N, M = map(int, input().split())                  # N 출석 번호 / M 신청서
+    arr = list(map(int, input().split()))             # 신청서
+
+    team = 0
+
+    leader = [0] * (N + 1)
+
+    for i in range(len(leader)):
+        leader[i] = i
+
+    for i in range(0, len(arr), 2):
+        leader[arr[i+1]] = arr[i]
+
+    for i in range(1, len(leader)):
+        if leader[i] == i:
+            team += 1
+
+    print(f"#{test_case} {team}")
 
     # ///////////////////////////////////////////////////////////////////////////////////
