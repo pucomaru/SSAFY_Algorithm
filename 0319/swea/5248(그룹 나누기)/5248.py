bae28a06e7e88854a3ea814e86f7453b181073ae
daye <myannnng@gmail.com>
2025-03-19 21:56:05 +0900

    0319

--- 0319/swea/5248(그룹 나누기)/5248.py
+++ 0319/swea/5248(그룹 나누기)/5248.py
@@ -2,7 +2,22 @@
 import sys
 sys.stdin = open("sample_input.txt", "r")
 
+def find_set(x):
+    while leader[x] != x:
+        leader[x] = leader[leader[x]]
+        x = leader[x]
+    return x
 
+def union(x,y):
+    global leader
+
+    ref_x = find_set(x)
+    ref_y = find_set(y)
+
+    if ref_x <= ref_y:
+        leader[ref_y] = ref_x
+    else:
+        leader[ref_x] = ref_y
 
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
@@ -12,20 +27,20 @@ for test_case in range(1, T + 1):
     N, M = map(int, input().split())                  # N 출석 번호 / M 신청서
     arr = list(map(int, input().split()))             # 신청서
 
-    team = 0
-
     leader = [0] * (N + 1)
 
-    for i in range(len(leader)):
+    for i in range(N+1):
         leader[i] = i
 
-    for i in range(0, len(arr), 2):
-        leader[arr[i+1]] = arr[i]
+    for i in range(0,len(arr),2):
+        union(arr[i],arr[i+1])
 
-    for i in range(1, len(leader)):
-        if leader[i] == i:
-            team += 1
+    different = []
 
-    print(f"#{test_case} {team}")
+    for i in range(1,len(leader)):
+        if leader[i] not in different:
+            different.append(leader[i])
+
+    print(f"#{test_case} {len(different)}")
 
     # ///////////////////////////////////////////////////////////////////////////////////
