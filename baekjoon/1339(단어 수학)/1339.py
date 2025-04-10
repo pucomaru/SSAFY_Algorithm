d039a4600d4d72e55e5f92f30d0e8539dfdcd8ff
pucomaru <myannnng@gmail.com>
2025-04-10 09:44:02 +0900

    Solved with GPT

--- baekjoon/1339(단어 수학)/1339.py
+++ baekjoon/1339(단어 수학)/1339.py
@@ -1,22 +1,43 @@
-def dfs(num)
+
 # N : 단어의 개수 
 N = int(input())
 
-word_1 = input()
-word_2 = input()
+# N개의 단어
+words = []
+for i in range(N):
+    words.append(input())
 
-visited = [0] * 10
-
+# 입력받은 알파벳들 ( 중복 x)
 alphabet = [] 
-for w in word_1:
-    if w not in alphabet:
-        alphabet.append(w)
+for word in words:
+    for idx in range(len(word)):
+        if word[idx] not in alphabet:
+            alphabet.append(word[idx])
 
-for w in word_2:
-    if w not in alphabet:
-        alphabet.append(w)
+# 알파벳 가중치 계산
+weights = {}
+for word in words:
+    length = len(word)
+    for i, ch in enumerate(word):
+        power = length - i - 1
+        weights[ch] = weights.get(ch,0) + (10 ** power)
 
-number = [9,8,7,6,5,4,3,2,1,0]
-use_alphabet = number[:len(alphabet)]
+# 가중치 기준 내림차순 정렬
+sorted_weights = sorted(weights.items(), key = lambda x : x[1], reverse=True)
 
-num_dic = {}
+# 숫자 매핑 ( 큰 가중치 -> 큰 숫자)
+alphabet_to_digit = {}
+digit = 9
+for ch, _ in sorted_weights:
+    alphabet_to_digit[ch] = digit
+    digit -= 1
+
+# 단어 숫자 변환 및 합 계산
+result = 0
+for word in words:
+    num_str = ''
+    for ch in word:
+        num_str += str(alphabet_to_digit[ch])
+    result += int(num_str)
+
+print(result)
